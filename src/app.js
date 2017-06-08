a67749b1f0e55988316024fceb5dea43c08e0992
Gary Shirk <garyshirk@yahoo.com>
2017-06-08 17:04:26 -0500

    completed auth sample (Sections 9 and 10)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+	state = { loggedIn: null };
 
 	componentWillMount() {
 		firebase.initializeApp({
@@ -13,14 +15,42 @@ class App extends Component {
 			databaseURL: 'https://authsample-ab929.firebaseio.com',
 			projectId: 'authsample-ab929',
 			storageBucket: 'authsample-ab929.appspot.com',
-			messagingSenderId: '369657555837' });
+			messagingSenderId: '369657555837' 
+		});
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (
+					<Card>
+						<CardSection>
+							<Button onPress={() => firebase.auth().signOut()}>
+								Logout
+							</Button>
+						</CardSection>
+					</Card>
+				);
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size="large" />;
+		}
 	}
 
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 		);
 	}
