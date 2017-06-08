7fee87bd9d24e117d33384f9a325074e6a2d6503
Gary Shirk <garyshirk@yahoo.com>
2017-06-08 15:13:35 -0500

    Handling successful and failed login events to firebase - sec 10-74

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyCw-cuEjand7pg1Rl9xy2P0wdJuX_AirjU',
+			authDomain: 'authsample-ab929.firebaseapp.com',
+			databaseURL: 'https://authsample-ab929.firebaseio.com',
+			projectId: 'authsample-ab929',
+			storageBucket: 'authsample-ab929.appspot.com',
+			messagingSenderId: '369657555837' });
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>An App</Text>
+				<LoginForm />
 			</View>
 		);
 	}
